ccdfae72ec3cd7fa8c14d27b759da0e4ec892159
stevietodd <stevie.schraudner@gmail.com>
2025-11-07 14:00:59 -0500

    Use MySQL for work

--- CpuQuinticFirstWithBreakoutsChecker.cpp
+++ CpuQuinticFirstWithBreakoutsChecker.cpp
@@ -141,6 +141,7 @@ std::vector<int*>* CpuQuinticFirstWithBreakoutsChecker::findHits(
 		doubleTol = getDoublePrecisionBasedOnMaxValue(maxValue);
 
         for (int v = loopStartEnds[2]; v <= loopStartEnds[3]; v++) {
+			if (std::abs(v) % 1'000 == 0) printf("v=%d (about 10%% done)\n", v);
             v4 = v5 + ((v < 0) ? -LUT[-v] : LUT[v]) * theConst4f;
             if (v4 < v4BreakoutLow || v4 > v4BreakoutHigh) {
 				// we can't possibly get back to the needle, so bust out
