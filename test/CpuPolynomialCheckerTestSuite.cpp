a33d1f83f4d6ee34f3d5fea27bbd47cc18d85fb2
stevietodd <stevie.schraudner@gmail.com>
2026-09-18 23:31:59 -0400

    Add back CPU test suite comments

--- test/CpuPolynomialCheckerTestSuite.cpp
+++ test/CpuPolynomialCheckerTestSuite.cpp
@@ -24,8 +24,15 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticLastOnlyQuinticQuarticResultsConfirmT
 
     hits = checker->findHits(ZETA5, M_PI, 5, NULL, loopRanges, doubleHitCount);
 
+	// testing indicates there should be at least 21,445 float hits within tolerance range...
+	//ASSERT_LE(171'720, floatHitCount);
+
+    // ...but there should be closer to 26-28 depending on rounding. If there are too many hits something is wrong
+    //EXPECT_GE(171'722, floatHitCount);
+
+	// ...however there should be 0 "real" double hits
 	ASSERT_EQ(0, doubleHitCount);
-	ASSERT_EQ(doubleHitCount, static_cast<long>(hits->size()));
+	ASSERT_EQ(0, hits->size());
 
     // now let's ensure some close hits (err <= .000003) were returned
     // bool hit1Found = false, hit2Found = false, hit3Found = false;
@@ -71,8 +78,16 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticLastZeroAndOneHighDegreesResultsConfi
 
     hits = checker->findHits(ZETA5, M_PI, 5, NULL, loopRanges, doubleHitCount);
 
+	// testing indicates there should be at least 8,940 float hits within tolerance range...
+	//ASSERT_LE(8'940, floatHitCount);
+	//ASSERT_LE(74, floatHitCount); // why the change??
+	//ASSERT_LE(66'761, floatHitCount); // why the change again??
+	
+	//ASSERT_GE(534'940, floatHitCount);
+
+    // ...however there should be 0 "real" double hits
 	ASSERT_EQ(0, doubleHitCount);
-	ASSERT_EQ(doubleHitCount, static_cast<long>(hits->size()));
+	ASSERT_EQ(0, hits->size());
 
     // // now let's ensure some close hits (err <= .000003) were returned
     // bool hit1Found = false, hit2Found = false, hit3Found = false;
@@ -117,8 +132,8 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticLastZeta4WithPiTest) {
     hits = checker->findHits(ZETA4, M_PI, 5, NULL, loopRanges, doubleHitCount);
 
 	// should find the real hit when zeta4 = (pi^4) / 90
-	ASSERT_EQ(1, hits->size());
 	ASSERT_EQ(1, doubleHitCount);
+	ASSERT_EQ(1, hits->size());
 }
 
 TEST(CpuPolynomialCheckerTestSuite, QuinticFirstOnlyQuinticQuarticResultsConfirmTest) {
@@ -135,9 +150,19 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticFirstOnlyQuinticQuarticResultsConfirm
 	};
 
     hits = checker->findHits(ZETA5, M_PI, 5, NULL, loopRanges, doubleHitCount);
+	// testing indicates there should be at least 8,940 float hits within tolerance range...
+	//ASSERT_LE(8'940, floatHitCount);
+	//ASSERT_LE(74, floatHitCount); // why the change??
+	//ASSERT_LE(171'720, floatHitCount); // why the change again??
 
+    // ...but there should be closer to 8,949 float hits depending on rounding. If there are too many something is wrong
+    //EXPECT_GE(8'955, floatHitCount);
+	//ASSERT_GE(74, floatHitCount); // why the change??
+	//ASSERT_GE(171'722, floatHitCount); // why the change again??
+
+	// ...however there should be 0 "real" double hits
 	ASSERT_EQ(0, doubleHitCount);
-	ASSERT_EQ(doubleHitCount, static_cast<long>(hits->size()));
+	ASSERT_EQ(0, hits->size());
 
     // now let's ensure some close hits (err <= .000003) were returned
     // bool hit1Found = false, hit2Found = false, hit3Found = false;
@@ -182,8 +207,8 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticFirstZeta4WithPiTest) {
     hits = checker->findHits(ZETA4, M_PI, 5, NULL, loopRanges, doubleHitCount);
 
 	// should find the real hit when zeta4 = (pi^4) / 90
-	ASSERT_EQ(1, hits->size());
 	ASSERT_EQ(1, doubleHitCount);
+	ASSERT_EQ(1, hits->size());
 }
 
 TEST(CpuPolynomialCheckerTestSuite, QuinticFirstWithBreakoutsOnlyQuinticQuarticResultsConfirmTest) {
@@ -199,9 +224,19 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticFirstWithBreakoutsOnlyQuinticQuarticR
 	};
 
     hits = checker->findHits(ZETA5, M_PI, 5, NULL, loopRanges, doubleHitCount);
+	// testing indicates there should be at least 8,940 float hits within tolerance range...
+	//ASSERT_LE(8'940, floatHitCount);
+	//ASSERT_LE(74, floatHitCount); // why the change??
+	//ASSERT_LE(171'720, floatHitCount); // why the change again??
 
+    // ...but there should be closer to 8,949 float hits depending on rounding. If there are too many something is wrong
+    //EXPECT_GE(8'955, floatHitCount);
+	//ASSERT_GE(74, floatHitCount); // why the change??
+	//ASSERT_GE(171'722, floatHitCount); // why the change again??
+
+	// ...however there should be 0 "real" double hits
 	ASSERT_EQ(0, doubleHitCount);
-	ASSERT_EQ(doubleHitCount, static_cast<long>(hits->size()));
+	ASSERT_EQ(0, hits->size());
 
     // now let's ensure some close hits (err <= .000003) were returned
     // bool hit1Found = false, hit2Found = false, hit3Found = false;
@@ -246,8 +281,8 @@ TEST(CpuPolynomialCheckerTestSuite, QuinticFirstWithBreakoutsZeta4WithPiTest) {
     hits = checker->findHits(ZETA4, M_PI, 5, NULL, loopRanges, doubleHitCount);
 
 	// should find the real hit when zeta4 = (pi^4) / 90
-	ASSERT_EQ(1, hits->size());
 	ASSERT_EQ(1, doubleHitCount);
+	ASSERT_EQ(1, hits->size());
 }
 
 TEST(CpuPolynomialCheckerTestSuite, QuinticFirstWithBreakoutsSameAsNonBreakoutsTest) {
